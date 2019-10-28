96dd172625a6ee54d6115c07890c42e54b0537ec
bowen <bowen.huang@yale.edu>
2019-10-28 03:47:18 -0400

    fix ostype bug

--- perl/setup.sh
+++ perl/setup.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-DIR=$(cd $(dirname "${BASH_SOURCE[0]}") >/dev/null && pwd)
+DIR=''
+case $OSTYPE in
+    linux-gnu*)
+        DIR=$(cd $(dirname "${BASH_SOURCE[0]}") >/dev/null && pwd)
+        echo "OSTYPE is Linux"
+        ;;
+    darwin*)
+        DIR=$(cd "$(dirname "$0")";pwd)
+        echo "OSTYPE is MacOSX"
+        ;;
+    *)
+        echo $OSTYPE
+        ;;
+esac
+
 echo "export VERONICA_PERL=$DIR"
 export VERONICA_PERL=$DIR
