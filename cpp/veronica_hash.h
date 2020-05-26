5c7ac21a729392afa1846c963c29ccd2ee19dc15
bowen <bowen.huang@yale.edu>
2020-05-25 23:16:24 -0400

    add type

--- cpp/veronica_hash.h
+++ cpp/veronica_hash.h
@@ -1,9 +1,11 @@
-#include <stdint.h>
+#pragma once
+
+#include "veronica_type.h"
 
 namespace veronica
 {
-    uint64_t int_hash(const uint64_t input)
+    uint64 hash_within_int(const uint64 input)
     {
-        return (input * 16807) % 2147483647;
+        return (input * 16807) % (uint64)2147483647;
     }
 }
--- cpp/veronica_time.h
+++ cpp/veronica_time.h
@@ -1,11 +1,13 @@
+#pragma once
+
 #include <ctime>
 #include <cmath>
 #include <string>
-#include <stdint.h>
+#include "veronica_type.h"
 
 namespace veronica
 {
-    const uint64_t MAX_NUM_TIMER = 10;
+    const uint64 MAX_NUM_TIMER = 10;
 
     struct timepoint
     {
@@ -13,11 +15,11 @@ namespace veronica
         struct timespec end;
     }timepoints[MAX_NUM_TIMER];
 
-    void check_timer_index(const uint64_t index)
+    void check_timer_index(const uint64 index)
     {
         if(index >= MAX_NUM_TIMER)
         {
-            printf("[error] timer index is larger than MAX_NUM_TIMER %lld\n", MAX_NUM_TIMER);
+            printf("[error] timer index is larger than MAX_NUM_TIMER %llu\n", MAX_NUM_TIMER);
             exit(-1);
         }
     }
@@ -27,44 +29,44 @@ namespace veronica
         return tv->tv_sec * pow(10,9) + tv->tv_nsec;
     }
 
-    void set_timer_start(const uint64_t index)
+    void set_timer_start(const uint64 index)
     {
         check_timer_index(index);
         clock_gettime(CLOCK_MONOTONIC, &(timepoints[index].start));
     }
 
-    void set_timer_end(const uint64_t index)
+    void set_timer_end(const uint64 index)
     {
         check_timer_index(index);
         clock_gettime(CLOCK_MONOTONIC, &(timepoints[index].end));
     }
 
-    void print_timer(const uint64_t index, const char* name)
+    void print_timer(const uint64 index, const char* name)
     {
         double elapsed_time = time_spec_to_ns(&(timepoints[index].end)) - time_spec_to_ns((&timepoints[index].start));
 
         if(elapsed_time >= pow(10,9))
         {
-            uint64_t sec  = floor(elapsed_time / pow(10,9));
-            uint64_t msec = floor((elapsed_time - sec * pow(10,9)) / pow(10,6));
-            printf("[time] timer %s = %llds %lldms\n", name, sec, msec);
+            uint64 sec  = floor(elapsed_time / pow(10,9));
+            uint64 msec = floor((elapsed_time - sec * pow(10,9)) / pow(10,6));
+            printf("[time] timer %s = %llus %llums\n", name, sec, msec);
         }
         else if(elapsed_time >= pow(10,6))
         {
-            uint64_t msec  = floor(elapsed_time / pow(10,6));
-            uint64_t usec = floor((elapsed_time - msec * pow(10,6)) / pow(10,3));
-            printf("[time] timer %s = %lldms %lldus\n", name, msec, usec);
+            uint64 msec  = floor(elapsed_time / pow(10,6));
+            uint64 usec = floor((elapsed_time - msec * pow(10,6)) / pow(10,3));
+            printf("[time] timer %s = %llums %lluus\n", name, msec, usec);
         }
         else if(elapsed_time >= pow(10,3))
         {
-            uint64_t usec  = floor(elapsed_time / pow(10,3));
-            uint64_t nsec = floor((elapsed_time - usec * pow(10,3)));
-            printf("[time] timer %s = %lldus %lldns\n", name, usec, nsec);
+            uint64 usec  = floor(elapsed_time / pow(10,3));
+            uint64 nsec = floor((elapsed_time - usec * pow(10,3)));
+            printf("[time] timer %s = %lluus %lluns\n", name, usec, nsec);
         }
         else
         {
-            printf("[time] timer %s = %lldns\n", name, (uint64_t)time_spec_to_ns(&(timepoints[index].start)));
-            printf("[time] timer %s = %lldns\n", name, (uint64_t)time_spec_to_ns(&(timepoints[index].end)));
+            printf("[time] timer %s = %lluns\n", name, (uint64)time_spec_to_ns(&(timepoints[index].start)));
+            printf("[time] timer %s = %lluns\n", name, (uint64)time_spec_to_ns(&(timepoints[index].end)));
         }
     }
 }
