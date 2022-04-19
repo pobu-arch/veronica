646c383b1a967166b9e8eadd1bd49f1708d49ad1
pobu-arch <huangpobu@163.com>
2022-04-19 12:16:29 -0400

    update timimg

--- cpp/veronica_time.h
+++ cpp/veronica_time.h
@@ -2,41 +2,29 @@
 
 #include <cstdio>
 #include <cmath>
+#include <chrono>
 #include <sys/time.h>
 #include "veronica_type.h"
+#include "veronica_asm.h"
+
+using namespace std;
 
 namespace veronica
 {
     const uint64 MAX_NUM_TIMER = 16;
-    
-    // cycle-level precision timer
-    struct cycle_pair
-    {
-        uint64 start;
-        uint64 end;
-    };
+          uint64 CPU_FREQ      = 0;
 
     // generic timer
     struct time_pair
     {
-        struct timeval start;
-        struct timeval end;
-    };
-
-    // for cycle-level precision
-    static time_pair  time_pair_array[MAX_NUM_TIMER];
-    // generic timer
-    static cycle_pair cycle_pair_array[MAX_NUM_TIMER];
-
-#if defined(ISA_X86_64)
-    inline uint64_t x86_rdtsc() 
-    { 
-        uint32_t lo, hi; 
-        asm volatile("mfence"); 
-        asm volatile("rdtsc" : "=a" (lo), "=d" (hi)); 
-        return (uint64_t)hi << 32 | lo; 
-    }
-#endif
+        #if defined ISA_X86_64
+            chrono::time_point<chrono::steady_clock> start;
+            chrono::time_point<chrono::steady_clock> end;
+        #else
+            struct timeval start;
+            struct timeval end;
+        #endif
+    }time_pair_array[MAX_NUM_TIMER];
 
     void check_timer_index(const int index)
     {
@@ -50,10 +38,12 @@ namespace veronica
     void set_timer_start(const int index)
     {
         check_timer_index(index);
-        #if defined(ISA_X86_64)
-            cycle_pair_array[index].start = x86_rdtsc();
+        
+        #if defined ISA_X86_64
+            MFENCE;
+            time_pair_array[index].start = std::chrono::steady_clock::now();
         #else
-            #if defined(ISA_ARM64)
+            #if defined ISA_ARM64
                 asm volatile("dmb ish": : :"memory");
             #endif
             gettimeofday(&(time_pair_array[index].start), NULL);
@@ -63,41 +53,56 @@ namespace veronica
     void set_timer_end(const int index)
     {
         check_timer_index(index);
-        #if defined(ISA_X86_64)
-            cycle_pair_array[index].end = x86_rdtsc();
+        #if defined ISA_X86_64
+            MFENCE;
+            time_pair_array[index].end = std::chrono::steady_clock::now();
         #else
-            #if defined(ISA_ARM64)
+            #if defined ISA_ARM64
                 asm volatile("dmb ish": : :"memory");
             #endif
             gettimeofday(&(time_pair_array[index].end), NULL);
         #endif
     }
 
-    double cycle_count_to_nsec(const uint64 cycle)
+    double nsec_to_cycle(const double nano_secs)
     {
-        uint64 cpu_freq = get_cpu_freq();
-        if(cpu_freq == 0)
+        if(CPU_FREQ == 0)
         {
-            cout << "[error] getting a 0 for cpu freq" << endl;
-            exit(-1);
+            CPU_FREQ = get_cpu_freq();
+            if(CPU_FREQ == 0)
+            {
+                cout << "[error] getting a 0 for cpu freq" << endl;
+                exit(-1);
+            }
+            else
+            {
+                //cout << "[info] cpu freq is " << cpu_freq << endl;
+            }
         }
-        double ns = pow(10.0, 9.0) * cycle / cpu_freq;
-        return ns;
+        
+        double cycles = nano_secs * CPU_FREQ / pow(10.0, 9.0);
+        return cycles;
     }
 
     double get_elapsed_time_in_cycle(const int index)
     {
-        return (cycle_pair_array[index].end - cycle_pair_array[index].start);
+        check_timer_index(index);
+        #if defined ISA_X86_64
+            double nano_secs = chrono::duration_cast<chrono::nanoseconds>(time_pair_array[index].end - 
+                                                                          time_pair_array[index].start).count();
+            return nsec_to_cycle(nano_secs);
+        #endif
     }
 
     double get_elapsed_time_in_usec(const int index)
     {
-        #if defined(ISA_X86_64)
-            return (cycle_count_to_nsec(cycle_pair_array[index].end - cycle_pair_array[index].start)) / 1000;
+        #if defined ISA_X86_64
+            return chrono::duration_cast<chrono::microseconds>(time_pair_array[index].end - 
+                                                               time_pair_array[index].start).count();
         #else
-            timeval* tv       = &(veronica::time_pair_array[index].start);
+            timeval* tv       = &(time_pair_array[index].start);
             double start_time = tv->tv_sec * pow(10.0,6.0) + tv->tv_usec;
-                     tv       = &(veronica::time_pair_array[index].end);
+                     tv       = &(time_pair_array[index].end);
             double end_time   = tv->tv_sec * pow(10.0,6.0) + tv->tv_usec;
             return end_time - start_time;
         #endif
