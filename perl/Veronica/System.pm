b829b139651bab2fbdceb2862df2cbfa11c04457
pobu-arch <huangpobu@163.com>
2025-02-19 18:46:34 +0800

    update print_sys_info for perl

--- perl/Veronica/System.pm
+++ perl/Veronica/System.pm
@@ -233,15 +233,16 @@ sub print_sys_info
 {
     my ($compiler) = @_;
 
+    my $os_type                 = get_os_type();
+    my $compiler_type           = get_compiler_type($compiler);
+    my $host_isa_type           = get_host_isa_type();
+    my $target_isa_type         = get_target_isa_type($compiler);
     my $endian                  = get_endian();
     my $thp_status              = get_thp_status();
     my $page_size               = get_page_size();
     my $cache_line_size         = get_cache_line_size();
-    my $os_type                 = get_os_type();
-    my $host_isa_type           = get_host_isa_type();
-    my $target_isa_type         = get_target_isa_type($compiler);
-    my $num_physical_core       = get_num_physical_core_per_socket();
     my $num_threads_per_core    = get_threads_per_core();
+    my $num_physical_core       = get_num_physical_core_per_socket();
 
     &Veronica::Common::log_level("\n", 0);
     &Veronica::Common::log_level("this machine is $endian-endian", 3);
@@ -249,6 +250,7 @@ sub print_sys_info
     &Veronica::Common::log_level("page size is ".($page_size/1024)." KB, cache line size is $cache_line_size bytes", 3);
     &Veronica::Common::log_level("host OS is $os_type, host ISA is $host_isa_type, target ISA is $target_isa_type", 3);
     &Veronica::Common::log_level("there are $num_physical_core physical cores per socket, and $num_threads_per_core threads per physical core", 3);
+    &Veronica::Common::log_level("using $compiler_type for compilation", 3);
     &Veronica::Common::log_level("\n", 0);
 }
 1;
