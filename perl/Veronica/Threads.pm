749b2db36099010fd736d695079cc11b05a40d15
pobu <huangpobu@163.com>
2025-05-26 13:08:12 +0800

    update format

--- perl/Veronica/Threads.pm
+++ perl/Veronica/Threads.pm
@@ -152,6 +152,7 @@ sub join_n_thread_with_log
 
         if($error)
         {
+            &Veronica::Common::log_level("\n", 0);
             &Veronica::Common::log_level('Evicting all the threads due to previous detected error', 1);
             
             # empty the thread pool
