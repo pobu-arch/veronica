3c58d5ab794baa27f986a4d373ae69c231bae096
pobu-arch <huangpobu@163.com>
2026-01-06 15:53:38 +0800

    update print info

--- perl/Veronica/System.pm
+++ perl/Veronica/System.pm
@@ -15,6 +15,122 @@ use lib "$ENV{'VERONICA'}/perl";
 use Veronica::Common;
 use Veronica::Threads;
 
+sub get_endian
+{
+    my $result = `echo -n I | od -o | head -n1 | cut -f2 -d" " | cut -c6`;
+    return $result ? 'little' : 'big';
+}
+
+sub get_thp_status
+{
+    my $path = "/sys/kernel/mm/transparent_hugepage/enabled";
+    my $result = 'undetectable';
+    $result = `cat $path` if -e $path;
+    chomp $result;
+    return "< $result >";
+}
+
+sub get_page_size
+{
+    my $result = `getconf PAGESIZE`;
+    chomp $result;
+    return $result if $result =~ /\d+/;
+    return 0; # default value
+}
+
+sub get_cache_line_size
+{
+    my $os_type = get_os_type();
+    my $result  = 0;
+    if($os_type eq 'MACOSX')
+    {
+        $result = `sysctl hw.cachelinesize`;
+    }
+    elsif($os_type eq 'LINUX')
+    {
+        my $path = "/sys/devices/system/cpu/cpu0/cache/index0/coherency_line_size";
+        $result = `cat $path` if -e $path;
+    }
+    else
+    {
+        &Veronica::Common::log_level('unsupported OS', -1);
+    }
+    
+    return $+{num} if($result =~ /(?<num>\d+)/);
+    return 0; # default value
+}
+
+sub get_threads_per_core
+{
+    my $os_type = get_os_type();
+    if($os_type eq 'MACOSX')
+    {
+        my $num_logical_cores_per_socket  = get_num_logical_core_per_socket();
+        my $num_physical_cores_per_socket = get_num_physical_core_per_socket();
+        if($num_physical_cores_per_socket != 0)
+        {
+            return $num_logical_cores_per_socket / $num_physical_cores_per_socket;
+        }
+        else
+        {
+            # use this as the default value if the detection failed
+            return 1;
+        }
+    }
+    elsif($os_type eq 'LINUX')
+    {
+        my $result = `lscpu`;
+        my $threads_per_core = 1; # default value
+           $threads_per_core = $+{num} if ($result =~ /Thread\(s\)\s+per\s+core\:\s+(?<num>\d+)/g);
+        return $threads_per_core;
+    }
+    else
+    {
+        &Veronica::Common::log_level('unsupported OS', -1);
+    }
+}
+
+sub get_num_physical_core_per_socket
+{
+    my $os_type = get_os_type();
+    if($os_type eq 'MACOSX')
+    {
+        my $result = `sysctl hw.physicalcpu`;
+        return $+{num} if($result =~ /(?<num>\d+)/g);
+        return 0; # default value
+    }
+    elsif($os_type eq 'LINUX')
+    {
+        my $result = `lscpu`;
+        return $+{num} if($result =~ /Core\(s\)\s+per\s+socket\:\s+(?<num>\d+)/g);
+        return 0; # default value
+    }
+    else
+    {
+        &Veronica::Common::log_level('unsupported OS', -1);
+    }
+}
+
+sub get_num_logical_core_per_socket
+{
+    my $os_type = get_os_type();
+    if($os_type eq 'MACOSX')
+    {
+        my $result = `sysctl hw.logicalcpu`;
+        return $+{num} if($result =~ /(?<num>\d+)/g);
+        return 0; # default value
+    }
+    elsif($os_type eq 'LINUX')
+    {
+        return (&Veronica::System::get_threads_per_core() * 
+                &Veronica::System::get_num_physical_core_per_socket());
+    }
+    else
+    {
+        &Veronica::Common::log_level('unsupported OS', -1);
+    }
+}
+
 sub get_os_type
 {
     my $result = `uname -a`;
@@ -31,6 +147,7 @@ sub get_os_type
         &Veronica::Common::log_level('unsupported OS', -1);
     }
 }
+
 
 sub get_compiler_type
 {
@@ -160,145 +277,37 @@ sub get_target_isa_type
     return parse_isa_type($isa);
 }
 
-sub get_endian
+sub get_perf_governor
 {
-    my $result = `echo -n I | od -o | head -n1 | cut -f2 -d" " | cut -c6`;
-    return $result ? 'little' : 'big';
-}
-
-sub get_thp_status
-{
-    my $path = "/sys/kernel/mm/transparent_hugepage/enabled";
-    my $result = 'undetectable';
-    $result = `cat $path` if -e $path;
-    chomp $result;
-    return "< $result >";
-}
-
-sub get_page_size
-{
-    my $result = `getconf PAGESIZE`;
-    chomp $result;
-    return $result if $result =~ /\d+/;
-    return 0; # default value
-}
-
-sub get_cache_line_size
-{
-    my $os_type = get_os_type();
-    my $result  = 0;
-    if($os_type eq 'MACOSX')
-    {
-        $result = `sysctl hw.cachelinesize`;
-    }
-    elsif($os_type eq 'LINUX')
-    {
-        my $path = "/sys/devices/system/cpu/cpu0/cache/index0/coherency_line_size";
-        $result = `cat $path` if -e $path;
-    }
-    else
-    {
-        &Veronica::Common::log_level('unsupported OS', -1);
-    }
-    
-    return $+{num} if($result =~ /(?<num>\d+)/);
-    return 0; # default value
-}
-
-sub get_threads_per_core
-{
-    my $os_type = get_os_type();
-    if($os_type eq 'MACOSX')
-    {
-        my $num_logical_cores_per_socket  = get_num_logical_core_per_socket();
-        my $num_physical_cores_per_socket = get_num_physical_core_per_socket();
-        if($num_physical_cores_per_socket != 0)
-        {
-            return $num_logical_cores_per_socket / $num_physical_cores_per_socket;
-        }
-        else
-        {
-            # use this as the default value if the detection failed
-            return 1;
-        }
-    }
-    elsif($os_type eq 'LINUX')
-    {
-        my $result = `lscpu`;
-        my $threads_per_core = 1; # default value
-           $threads_per_core = $+{num} if ($result =~ /Thread\(s\)\s+per\s+core\:\s+(?<num>\d+)/g);
-        return $threads_per_core;
-    }
-    else
-    {
-        &Veronica::Common::log_level('unsupported OS', -1);
-    }
-}
-
-sub get_num_physical_core_per_socket
-{
-    my $os_type = get_os_type();
-    if($os_type eq 'MACOSX')
-    {
-        my $result = `sysctl hw.physicalcpu`;
-        return $+{num} if($result =~ /(?<num>\d+)/g);
-        return 0; # default value
-    }
-    elsif($os_type eq 'LINUX')
-    {
-        my $result = `lscpu`;
-        return $+{num} if($result =~ /Core\(s\)\s+per\s+socket\:\s+(?<num>\d+)/g);
-        return 0; # default value
-    }
-    else
-    {
-        &Veronica::Common::log_level('unsupported OS', -1);
-    }
-}
-
-sub get_num_logical_core_per_socket
-{
-    my $os_type = get_os_type();
-    if($os_type eq 'MACOSX')
-    {
-        my $result = `sysctl hw.logicalcpu`;
-        return $+{num} if($result =~ /(?<num>\d+)/g);
-        return 0; # default value
-    }
-    elsif($os_type eq 'LINUX')
-    {
-        return (&Veronica::System::get_threads_per_core() * 
-                &Veronica::System::get_num_physical_core_per_socket());
-    }
-    else
-    {
-        &Veronica::Common::log_level('unsupported OS', -1);
-    }
+    return `cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor 2>/dev/null | sort -u | tr '\n' ' ' ; echo`;
 }
 
 sub print_sys_info
 {
     my ($compiler) = @_;
 
-    my $os_type                 = get_os_type();
     my $compiler_type           = get_compiler_type($compiler);
     my $compiler_version        = get_compiler_version($compiler);
-    my $host_isa_type           = get_host_isa_type();
-    my $target_isa_type         = get_target_isa_type($compiler);
     my $endian                  = get_endian();
+    my $perf_governor           = get_perf_governor();
     my $thp_status              = get_thp_status();
     my $page_size               = get_page_size();
     my $cache_line_size         = get_cache_line_size();
     my $num_threads_per_core    = get_threads_per_core();
     my $num_physical_core       = get_num_physical_core_per_socket();
+    my $os_type                 = get_os_type();
+    my $host_isa_type           = get_host_isa_type();
+    my $target_isa_type         = get_target_isa_type($compiler);
 
     &Veronica::Common::log_level("\n", 0);
+    &Veronica::Common::log_level("using $compiler_type $compiler_version for compilation", 3);
     &Veronica::Common::log_level("this machine is $endian-endian", 3);
-    &Veronica::Common::log_level("transparent huge page status is $thp_status", 3);
+    &Veronica::Common::log_level("performance governor is $perf_governor", 3) if $os_type eq 'LINUX';
+    &Veronica::Common::log_level("transparent huge page status is $thp_status", 3) if $os_type eq 'LINUX';
     &Veronica::Common::log_level("basic page size is ".($page_size/1024)." KB, cache line size is $cache_line_size bytes", 3);
     &Veronica::Common::log_level("host OS is $os_type, host ISA is $host_isa_type, target ISA is $target_isa_type", 3);
     &Veronica::Common::log_level("there are $num_physical_core physical cores per socket, and $num_threads_per_core threads per physical core", 3);
-    &Veronica::Common::log_level("using $compiler_type $compiler_version for compilation", 3);
+    
     &Veronica::Common::log_level("\n", 0);
 }
 1;
